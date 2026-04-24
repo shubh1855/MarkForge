6d747fbea76d295849e4370d00756b37e6feedd3
shubh1855 <paulshubhranil1855@gmail.com>
2026-04-24 20:55:22 +0530

    added function to convert raw string of markdown text to a list of textnode objects

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -109,3 +109,15 @@ def split_nodes_image(old_nodes):
             new_nodes.append(TextNode(text, TextType.TEXT))
 
     return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+    return nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,6 +7,7 @@ from inline_markdown import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 
@@ -124,6 +125,60 @@ class TestInlineMarkdown(unittest.TestCase):
 
         self.assertEqual(nodes[1].text_type, TextType.IMAGE)
         self.assertEqual(nodes[3].text_type, TextType.LINK)
+
+    def test_text_to_textnodes(self):
+        text = (
+            "This is **text** with an _italic_ word and a `code block` "
+            "and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) "
+            "and a [link](https://boot.dev)"
+        )
+
+        nodes = text_to_textnodes(text)
+
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image",
+                    TextType.IMAGE,
+                    "https://i.imgur.com/fJRm4Vk.jpeg",
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+        )
+
+    def test_plain_text(self):
+        text = "just normal text"
+        nodes = text_to_textnodes(text)
+
+        self.assertEqual(
+            nodes,
+            [TextNode("just normal text", TextType.TEXT)],
+        )
+
+    def test_only_bold(self):
+        text = "**bold**"
+        nodes = text_to_textnodes(text)
+
+        self.assertEqual(
+            nodes,
+            [TextNode("bold", TextType.BOLD)],
+        )
+
+    def test_mixed_types(self):
+        text = "`code` and **bold**"
+        nodes = text_to_textnodes(text)
+
+        self.assertEqual(nodes[0].text_type, TextType.CODE)
+        self.assertEqual(nodes[2].text_type, TextType.BOLD)
 
 
 if __name__ == "__main__":
