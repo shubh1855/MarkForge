537c9e1ae9e795271a6f44adc778d1bde1a779d5
shubh1855 <paulshubhranil1855@gmail.com>
2026-04-24 08:02:57 +0530

    added extraction of text and image URLs. Also added test cases for it

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -24,3 +25,15 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append((TextNode(part, text_type)))
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,11 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+)
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -49,6 +53,49 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertListEqual(
             [TextNode("bold", TextType.BOLD)],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")],
+            matches,
+        )
+
+    def test_extract_multiple_images(self):
+        text = "![one](url1) and ![two](url2)"
+        matches = extract_markdown_images(text)
+
+        self.assertListEqual(
+            [("one", "url1"), ("two", "url2")],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links("This is a [link](https://boot.dev)")
+        self.assertListEqual(
+            [("link", "https://boot.dev")],
+            matches,
+        )
+
+    def test_extract_multiple_links(self):
+        text = "[one](url1) and [two](url2)"
+        matches = extract_markdown_links(text)
+
+        self.assertListEqual(
+            [("one", "url1"), ("two", "url2")],
+            matches,
+        )
+
+    def test_links_ignore_images(self):
+        text = "![img](url1) and [link](url2)"
+        matches = extract_markdown_links(text)
+
+        self.assertListEqual(
+            [("link", "url2")],
+            matches,
         )
 
 
