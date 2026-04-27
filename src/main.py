9b7dea403ce12aaf662b9caf4221c048eddf5f7d
shubh1855 <paulshubhranil1855@gmail.com>
2026-04-27 15:28:26 +0530

    added code to hook and see if files in public exist. If yes delete it and create new ones by sourcing fromstatic directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_static(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    os.mkdir(dest)
+
+    _copy_recursive(src, dest)
+
+
+def _copy_recursive(src, dest):
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying file: {src_path} → {dest_path}")
+            shutil.copy(src_path, dest_path)
+
+        else:
+            print(f"Creating directory: {dest_path}")
+            os.mkdir(dest_path)
+            _copy_recursive(src_path, dest_path)
 
 
 def main():
-    my_text = TextNode("This is a text node", TextType.TEXT, "example.com")
-    print(my_text)
+    copy_static("static", "public")
 
 
-main()
+if __name__ == "__main__":
+    main()
